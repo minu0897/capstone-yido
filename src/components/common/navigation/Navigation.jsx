59c38571ca6ec3f7f62b497a428ccfb19b6763d9
홍렬 유 <ssubong1215@naver.com>
2024-04-07 21:06:13 +0900

    Nav 작업

--- src/components/common/navigation/Navigation.jsx
+++ src/components/common/navigation/Navigation.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Navigation = () => {
 
   return (
-    <div className='navigation'>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/Community'}>Community</Link>
-      <Link to={'/Note'}>Note</Link>
-      <Link to={'/Recommand'}>Recommand</Link>
-    </div>
+      <Container>
+        <Row>
+          <Col>
+            <Link to={'/'}>Home</Link>
+          </Col>
+          <Col>
+            <Link to={'/Community'}>Community</Link>
+          </Col>
+          <Col>
+            <Link to={'/Note'}>Note</Link>
+          </Col>
+          <Col>
+            <Link to={'/Recommand'}>Recommand</Link>
+          </Col>
+        </Row>
+      </Container>
+
+      
   );
 };
 
