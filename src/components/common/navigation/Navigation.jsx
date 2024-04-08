89260bb242b8d59598c685e8eab3d67ee4b103be
minu <minu0897@naver.com>
2024-04-08 13:40:18 +0900

    navigation update

--- src/components/common/navigation/Navigation.jsx
+++ src/components/common/navigation/Navigation.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css'
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+//import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 
 const Navigation = () => {
 
   return (
     <div className='navigation'>
       <Link to={'/'}>Home</Link>
-      <Link to={'/Community'}>Community</Link>
-      <Link to={'/Note'}>Note</Link>
       <Link to={'/Recommand'}>Recommand</Link>
+      <Link to={'/Note'}>Note</Link>
+      <Link to={'/Community'}>Community</Link>
     </div>
   );
 };
