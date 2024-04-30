ad2766fc66a591ffb69dcbdd68bcae470445dfc2
minu <minu0897@naver.com>
2024-05-01 02:10:31 +0900

    login

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -5,8 +5,25 @@ import './Signup.css'
 const Signup = () => {
 
   return (
-    <div className='Signup'>
+    <div className='signup'>
+      <span className='span-blod'>Create account</span>
       
+      <span>Your name</span>
+      <div className="form-group">
+        <input
+          type="text"
+          id="username"
+          name="username"
+          required
+        />
+      </div>
+      <span>Your ID</span>
+      <span>Mobile number</span>
+      <span>Email</span>
+      <span>Password</span>
+      <span>Re-enter password</span>
+
+
     </div>
   );
 };
