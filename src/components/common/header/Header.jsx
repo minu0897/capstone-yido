884c80075fab0815b28a61320d3896543137eb3c
hongry7210 <ssubong1215@naver.com>
2024-05-02 15:26:54 +0900

    0502 헤더작업

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -36,12 +36,12 @@ const Header = () => {
   return (  
     <div className='header-back'>
       <div className='header'>
-        <div>
+        <div style={{marginTop:'10px'}}>
         <Link to={'/'}>
           <img src={icon} width="40px" height="40px" />
         </Link>
         </div>
-        <div width="150px" style={{marginLeft:"13px"}}>
+        <div width="150px" style={{marginLeft:"13px", marginTop:"5px"}}>
           <div style={{color: 'white',width:"100px"}}>
             Yido
           </div>
@@ -57,19 +57,19 @@ const Header = () => {
           </button>
         </form>
 
-        <div style={{color:'white', marginLeft: '280px',width:"50px"}}>
+        <div style={{color:'white', marginLeft: '280px',width:"50px",marginTop:'5px'}}>
           <Link to={'/Login'}  style={{textDecoration:'none'}}>
             <a className="login-a">Sign In</a>
           </Link>
         </div>
         
-        <div style={{color:'white', marginLeft: '10px',width:"70px"}}>
+        <div style={{color:'white', marginLeft: '10px',width:"70px",marginTop:'5px'}}>
           <Link to={'/Signup'} style={{textDecoration:'none'}}>
             <a className="login-a">Sign Up</a>
           </Link>
         </div>
 
-        <div style={{ marginRight:"0px"}}>
+        <div style={{ marginRight:"0px", marginTop:'13px'}}>
           <img src="https://us.123rf.com/450wm/get4net/get4net1711/get4net171100432/89002328-%EC%A2%85-%EC%95%84%EC%9D%B4%EC%BD%98.jpg" width="20px" height="20px"/>
         </div>
       </div>
