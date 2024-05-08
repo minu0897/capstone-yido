cbeef5e7d1175818d0193d8b768f426b273f15e6
hongry7210 <ssubong1215@naver.com>
2024-05-09 01:09:19 +0900

    Recommand 수정

--- src/pages/Recommand/Recommand.jsx
+++ src/pages/Recommand/Recommand.jsx
@@ -11,13 +11,28 @@ const Recommand = () => {
     setVideos(videoData);
   }, []);
 
+  const recommandvideoarray={
+    display: 'flex',
+    justifyContent: 'center', // 중앙 정렬을 위해 추가
+    flexWrap: 'wrap', // 여러 줄로 나열되도록 처리
+    gap: '20px', // 각 아이템 간의 간격
+  };
+
+  const videolink={
+    textDecoration: 'none',
+    color: 'black',
+    width: '322px',
+  }
+
   return (
-    <div className='recommand'>
+    <div style={{marginTop:'20px'}}>
+    <div style={recommandvideoarray}>
       {videos.map(video => (
-        <Link to={`/videoplayer?id=${video.videoId}`} className="video-link" key={video.videoId}>
+        <Link to={`/videoplayer?id=${video.videoId}`} key={video.videoId} style={videolink}>
           <VideoContainer video={video} />
         </Link>
       ))}
+    </div>
     </div>
   );
 };
