a2d3490161353934a293542f7ee14e3455d836c8
hongry7210 <ssubong1215@naver.com>
2024-05-20 21:04:03 +0900

    community 수정

--- src/pages/Community/Card.jsx
+++ src/pages/Community/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css'; // Card 컴포넌트 전용 스타일시트
 
-const Card = ({ postId, title, content, likes, dislikes, tags }) => {
+const Card = ({ postId, title, content, likes, tags }) => {
   return (
     <div className="card">
       <img src="https://dummyimage.com/600x400/000/fff&text=photo" alt={title} className="card-image" />
@@ -16,7 +16,6 @@ const Card = ({ postId, title, content, likes, dislikes, tags }) => {
         </div>
         <div className="card-stats">
           <span className="card-likes">{likes} likes</span>
-          <span>{dislikes} dislikes</span>
         </div>
       </div>
     </div>
