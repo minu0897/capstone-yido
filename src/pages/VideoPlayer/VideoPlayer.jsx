ec8be4e494c292539baed2dc981132df35accdc7
minu <minu0897@naver.com>
2024-05-21 16:32:40 +0900

    video player

--- src/pages/VideoPlayer/VideoPlayer.jsx
+++ src/pages/VideoPlayer/VideoPlayer.jsx
@@ -7,34 +7,361 @@ import './VideoPlayer.css'
 import Player from '../../components/videoplayer/player/Player'
 import Subtitles from '../../components/videoplayer/subtitle/Subtitles'
 
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+import Slider from "@material-ui/core/Slider";
+import Tooltip from "@material-ui/core/Tooltip";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import VolumeUp from "@material-ui/icons/VolumeUp";
+import VolumeDown from "@material-ui/icons/VolumeDown";
+import VolumeMute from "@material-ui/icons/VolumeOff";
+import FullScreen from "@material-ui/icons/Fullscreen";
+import Popover from "@material-ui/core/Popover";
+//import screenful from "screenfull";
+import Controls from '../../components/videoplayer/control/Controls'
+
+
+//-------------------------------------------
+//함수 및 변수(player)
+//-------------------------------------------
+
+const useStyles = makeStyles((theme) => ({
+  playerWrapper: {
+    width: "100%",
+
+    position: "relative",
+    // "&:hover": {
+    //   "& $controlsWrapper": {
+    //     visibility: "visible",
+    //   },
+    // },
+  },
+
+  controlsWrapper: {
+    visibility: "hidden",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: "rgba(0,0,0,0.4)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  topControls: {
+    display: "flex",
+    justifyContent: "flex-end",
+    padding: theme.spacing(2),
+  },
+  middleControls: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomWrapper: {
+    display: "flex",
+    flexDirection: "column",
+
+    // background: "rgba(0,0,0,0.6)",
+    // height: 60,
+    padding: theme.spacing(2),
+  },
+
+  bottomControls: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    // height:40,
+  },
+
+  button: {
+    margin: theme.spacing(1),
+  },
+  controlIcons: {
+    color: "#777",
+
+    fontSize: 50,
+    transform: "scale(0.9)",
+    "&:hover": {
+      color: "#fff",
+      transform: "scale(1)",
+    },
+  },
+
+  bottomIcons: {
+    color: "#999",
+    "&:hover": {
+      color: "#fff",
+    },
+  },
+
+  volumeSlider: {
+    width: 100,
+  },
+}));
+
+const PrettoSlider = withStyles({
+  root: {
+    height: 8,
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+})(Slider);
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
+
+const format = (seconds) => {
+  if (isNaN(seconds)) {
+    return `00:00`;
+  }
+  const date = new Date(seconds * 1000);
+  const hh = date.getUTCHours();
+  const mm = date.getUTCMinutes();
+  const ss = date.getUTCSeconds().toString().padStart(2, "0");
+  if (hh) {
+    return `${hh}:${mm.toString().padStart(2, "0")}:${ss}`;
+  }
+  return `${mm}:${ss}`;
+};
+
+let count = 0;
+
 const VideoPlayer = () => {
-  const [currentTime, setCurrentTime] = useState(0);
+  //-------------------------------------------
+  //yido 변수
+  //-------------------------------------------
   const [data, setData] = useState(null);
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const videoId = searchParams.get('id');
-  const [played, setPlayed] = useState(0);
-  const [seeking, setSeeking] = useState(false);
-  const playerRef = useRef(null);
   const [subtitles, setSubtitles] = useState(null);
   const subtitlesRef = useRef(null); // 스크롤을 위한 ref 생성
-  const [playing, setPlaying] = useState(false); // 재생 상태를 제어할 상태 변수
 
+  //-------------------------------------------
+  //player 변수
+  //-------------------------------------------
+  const classes = useStyles();
+  // const [count, setCount] = useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [timeDisplayFormat, setTimeDisplayFormat] = React.useState("normal");
+  const [bookmarks, setBookmarks] = useState([]);
+  const [state, setState] = useState({
+    pip: false,
+    playing: true,
+    controls: false,
+    light: false,
+
+    muted: true,
+    played: 0,
+    duration: 0,
+    playbackRate: 1.0,
+    volume: 1,
+    loop: false,
+    seeking: false,
+  });
+
+  const playerRef = useRef(null);
+  const playerContainerRef = useRef(null);
+  const controlsRef = useRef(null);
+  const canvasRef = useRef(null);
+  const {
+    playing,
+    controls,
+    light,
+
+    muted,
+    loop,
+    playbackRate,
+    pip,
+    played,
+    seeking,
+    volume,
+  } = state;
+
+  
+  //-------------------------------------------
+  //player 함수
+  //-------------------------------------------
+  const handlePlayPause = () => {
+    setState({ ...state, playing: !state.playing });
+  };
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
+  };
+
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10);
+  };
+
+  const handleProgress = (changeState) => {
+    if (count > 3) {
+      //controlsRef.current.style.visibility = "visible";
+      count = 0;
+    }
+    //if (controlsRef.current.style.visibility == "visible") {
+      count += 1;
+    //}
+    if (!state.seeking) {
+      setState({ ...state, ...changeState });
+    }
+  };
+
+  const handleSeekChange = (e, newValue) => {
+    console.log({ newValue });
+    setState({ ...state, played: parseFloat(newValue / 100) });
+  };
+
+  const handleSeekMouseDown = (e) => {
+    setState({ ...state, seeking: true });
+  };
+
+  const handleSeekMouseUp = (e, newValue) => {
+    console.log({ value: e.target });
+    setState({ ...state, seeking: false });
+    // console.log(sliderRef.current.value)
+    playerRef.current.seekTo(newValue / 100, "fraction");
+  };
+
+  const handleDuration = (duration) => {
+    setState({ ...state, duration });
+  };
+
+  const handleVolumeSeekDown = (e, newValue) => {
+    setState({ ...state, seeking: false, volume: parseFloat(newValue / 100) });
+  };
+  const handleVolumeChange = (e, newValue) => {
+    // console.log(newValue);
+    setState({
+      ...state,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+
+  const toggleFullScreen = () => {
+    //screenful.toggle(playerContainerRef.current);
+  };
+
+  const handleMouseMove = () => {
+    console.log("mousemove");
+    controlsRef.current.style.visibility = "visible";
+    count = 0;
+  };
+
+  const hanldeMouseLeave = () => {
+    controlsRef.current.style.visibility = "visible";
+    count = 0;
+  };
+
+  const handleDisplayFormat = () => {
+    setTimeDisplayFormat(
+      timeDisplayFormat == "normal" ? "remaining" : "normal"
+    );
+  };
+
+  const handlePlaybackRate = (rate) => {
+    setState({ ...state, playbackRate: rate });
+  };
+
+  const hanldeMute = () => {
+    setState({ ...state, muted: !state.muted });
+  };
+
+  const addBookmark = () => {
+    const canvas = canvasRef.current;
+    canvas.width = 160;
+    canvas.height = 90;
+    const ctx = canvas.getContext("2d");
+
+    ctx.drawImage(
+      playerRef.current.getInternalPlayer(),
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    const dataUri = canvas.toDataURL();
+    canvas.width = 0;
+    canvas.height = 0;
+    const bookmarksCopy = [...bookmarks];
+    bookmarksCopy.push({
+      time: playerRef.current.getCurrentTime(),
+      display: format(playerRef.current.getCurrentTime()),
+      image: dataUri,
+    });
+    setBookmarks(bookmarksCopy);
+  };
+
+  const currentTime =
+    playerRef && playerRef.current
+      ? playerRef.current.getCurrentTime()
+      : "00:00";
+
+  const duration =
+    playerRef && playerRef.current ? playerRef.current.getDuration() : "00:00";
+  const elapsedTime =
+    timeDisplayFormat == "normal"
+      ? format(currentTime)
+      : `-${format(duration - currentTime)}`;
+
+  const totalDuration = format(duration);
+  
+
+//-------------------------------------------
+//yido 함수
+//-------------------------------------------
   useEffect(() => {
     const url = '/api/video/'+videoId;
 
     axios.get(url)
       .then(response => {
         setData(response.data);
+        console.log(response.data);
       })
       .catch(error => {
       });
 
       const timer = setTimeout(() => {
-        setPlaying(true); // 1초 후 재생 상태를 true로 설정
+        //setPlaying(true); // 1초 후 재생 상태를 true로 설정
       }, 1000);
   
-      return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
+      //return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 클리어
   },[]);
   
   useEffect(() => {
@@ -55,43 +382,24 @@ const VideoPlayer = () => {
       });
       setSubtitles(updatedSubtitles);
       
-      if (subtitlesRef.current) {
-        const element = subtitlesRef.current;
+      //if (subtitlesRef.current) {
+        //const element = subtitlesRef.current;
         // 자막 컨테이너의 스크롤 높이만큼 스크롤 위치 설정
-        element.scrollTop = element.scrollHeight;
-      }
+        //element.scrollTop = element.scrollHeight;
+      //}
     }
   }, [currentTime]);
 
-  const handleProgress = (progress) => {
-    setCurrentTime(progress.playedSeconds);
-    if (!seeking) {
-      setPlayed(progress.played);
-    }
-  };
-
-  const handleSeekMouseDown = e => {
-      setSeeking(true);
-  };
-
-  const handleSeekChange = e => {
-      setPlayed(parseFloat(e.target.value));
-  };
-
-  const handleSeekMouseUp = e => {
-      setSeeking(false);
-      // playerRef.current가 존재할 때만 seekTo를 호출
-      if (playerRef.current) {
-      playerRef.current.seekTo(parseFloat(e.target.value));
-      }
-  };
-
+//-------------------------------------------
+//render
+//-------------------------------------------
   return(
     <div className='videoplayer'>
       <h1>{ data!=null && data.title}</h1>
       <div style={{height:"500px",display:"flex",flexDirection:"row"}}>
         <div style={{height:"450px",width:"800px",paddingRight:"15px"}}>
           <div className="player">
+            {false &&
               <ReactPlayer
                   url={"http://101.235.73.77:8088/video/video/"+videoId+".mp4"}
                   onProgress={handleProgress}
@@ -100,20 +408,58 @@ const VideoPlayer = () => {
                   ref={playerRef}
                   playing={playing}   
                   width="100%"
-                  height="100%"
+                  height="50%"
               />
-              <input
-                type="range"
-                min={0}
-                max={1}
-                step="any"
-                value={played}
-                onMouseDown={handleSeekMouseDown}
-                onChange={handleSeekChange}
-                onMouseUp={handleSeekMouseUp}
-                style={{ width: '100%' }}
-              />
-              <progress max={1} value={played} style={{ width: '100%' }}></progress>
+
+            }
+            <ReactPlayer
+              //url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+              url="http://101.235.73.77:8088/video/video/36.mp4"
+              ref={playerRef}
+              width="100%"
+              height="50%"
+              pip={pip}
+              playing={playing}
+              controls={false}
+              light={light}
+              loop={loop}
+              playbackRate={playbackRate}
+              volume={volume}
+              muted={muted}
+              onProgress={handleProgress}
+              config={{
+                file: {
+                  attributes: {
+                    crossorigin: "anonymous",
+                  },
+                },
+              }}
+            />
+              {
+                <Controls
+                  ref={controlsRef}
+                  onSeek={handleSeekChange}
+                  onSeekMouseDown={handleSeekMouseDown}
+                  onSeekMouseUp={handleSeekMouseUp}
+                  onDuration={handleDuration}
+                  onRewind={handleRewind}
+                  onPlayPause={handlePlayPause}
+                  onFastForward={handleFastForward}
+                  playing={playing}
+                  played={played}
+                  elapsedTime={elapsedTime}
+                  totalDuration={totalDuration}
+                  onMute={hanldeMute}
+                  muted={muted}
+                  onVolumeChange={handleVolumeChange}
+                  onVolumeSeekDown={handleVolumeSeekDown}
+                  onChangeDispayFormat={handleDisplayFormat}
+                  playbackRate={playbackRate}
+                  onPlaybackRateChange={handlePlaybackRate}
+                  onToggleFullScreen={toggleFullScreen}
+                  volume={volume}
+                  onBookmark={addBookmark}/>
+              }
           </div>
         </div>
         <div style={{height:"450px",width:"370px",paddingLeft:"15px"}}>
