ef204489543d199d48d152675660134bad6f84f4
hongry7210 <ssubong1215@naver.com>
2024-05-21 19:55:18 +0900

    community작업

--- src/pages/Community/Card.jsx
+++ src/pages/Community/Card.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './Card.css'; // Card 컴포넌트 전용 스타일시트
 
+const truncateText = (text, maxLength) => {
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + '...';
+  }
+  return text;
+};
+
 const Card = ({ postId, title, content, likes, tags }) => {
   return (
     <div className="card-wrapper">
       <div className="card">
         <img src="https://dummyimage.com/600x400/000/fff&text=photo" alt={title} className="card-image" />
         <div className="card-content">
-          <h3 className="card-title">{title}</h3>
-          <p className="card-subtitle">{content}</p>
+          <h3 className="card-title">{truncateText(title, 20)}</h3>
+          <p className="card-subtitle">{truncateText(content, 30)}</p>
           <div className="card-tags" style={{ textDecoration: 'none', color: 'gray' }}>
             {tags.map((tag, index) => (
               <span key={index} className="card-tag">#{tag} </span>
