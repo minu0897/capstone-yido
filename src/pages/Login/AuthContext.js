d6234a29a573f4d265417a3c9c50779b19d55f62
hongry7210 <ssubong1215@naver.com>
2024-05-22 17:01:03 +0900

    노트북으로 작업, 회원가입으로 db에 넣기 성공

--- src/pages/Login/AuthContext.js
+++ src/pages/Login/AuthContext.js
@@ -1,17 +1,24 @@
-// AuthContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    // 페이지 로드 시 localStorage에서 사용자 정보를 가져와서 상태에 설정
+    const storedUser = localStorage.getItem('user');
+    return storedUser ? JSON.parse(storedUser) : null;
+  });
 
   const login = (userData) => {
     setUser(userData);
+    // 사용자 데이터를 localStorage에 저장
+    localStorage.setItem('user', JSON.stringify(userData));
   };
 
   const logout = () => {
     setUser(null);
+    // localStorage에서 사용자 정보 제거
+    localStorage.removeItem('user');
   };
 
   return (
