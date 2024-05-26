cdabebad42e8dc4e5e84701bdb1d01b8111642a4
홍렬 유 <ssubong1215@naver.com>
2024-05-27 01:22:33 +0900

    Commit

--- src/pages/Community/Card.jsx
+++ src/pages/Community/Card.jsx
@@ -23,10 +23,10 @@ const Card = ({ videoId, title, content, likes, tags, writer }) => {
           <img src={imageUrl} alt="thumbnail" className="card-image" style={{width:'300px', height:'200px'}}/>
         )}
         <div className="card-content">
-          <h3 className="card-title" style={{fontSize:'19px'}}>{truncateText(title, 15)}</h3>
+          <h3 className="card-title" style={{fontSize:'19px', marginTop:'20px'}}>{truncateText(title, 15)}</h3>
           <p className="card-subtitle" style={{fontSize:'13px'}}>{truncateText(content, contentMaxLength)}</p>
           <div className="card-tags" style={{ textDecoration: 'none', color: 'gray' }}>
-            <span className="card-tag">{truncateText(tagsString, 10)}</span>
+            <span className="card-tag">{truncateText(tagsString, 15)}</span>
           </div>
           <hr className="card-divider" />
           <div style={{display:'flex', justifyContent: 'space-between'}}>
